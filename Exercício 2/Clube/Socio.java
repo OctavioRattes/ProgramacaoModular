58b560bd023c52543e646e3677020ce5b724aefa
Octávio Rattes <112592766+OctavioRattes@users.noreply.github.com>
2023-09-27 17:22:23 -0300

    Classes do programa Clube

---
+++ Exercício 2/Clube/Socio.java
@@ -0,0 +1,19 @@
+package Clube;
+
+import java.io.*;
+import java.util.*;
+
+public class Socio extends Frequentador {
+
+	private static final int MAX_CONVITES = 4;
+	private Convidado[] convidados;
+
+	public Convite enviarConvite(Data data, Convidado convidado) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	private boolean verificarConvites() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+}
