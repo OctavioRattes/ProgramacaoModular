0177a9b76edd8cec48c04f5bd1d8fb4cf76a38b5
OctavioRattes <octaviorattes1979@gmail.com>
2026-03-13 08:32:26 -0300

    Exercicio5

---
+++ PM2026/Conta.java
@@ -0,0 +1,21 @@
+public class Conta {
+
+	private String cpf;
+	private String conta;
+	private double saldo;
+	private double limite;
+	private double taxa;
+
+	public double saque() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	public double deposito() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	public void calcularTaxa() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+}
