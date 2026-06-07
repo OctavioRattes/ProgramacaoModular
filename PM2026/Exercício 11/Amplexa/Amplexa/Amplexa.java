b7dc8bf8b02b84e0a710349697c8bd0eeaa93a38
OctavioRattes <octaviorattes1979@gmail.com>
2026-06-07 18:45:34 -0300

    Implemantação de métodos - Classe Amplexa

--- PM2026/Exercício 11/Amplexa/Amplexa/Amplexa.java
+++ PM2026/Exercício 11/Amplexa/Amplexa/Amplexa.java
@@ -13,8 +13,40 @@ public class Amplexa {
 		this.dispositivos = new ArrayList<>();
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
 	public void addDispositivos(Dispositivo novo) {
 		dispositivos.add(novo);
 	}
 
+	public boolean ligar(String nome) {
+		for (Dispositivo dispositivo : dispositivos) {
+			if (((Amplexa) dispositivos).getNome().equals(nome) && dispositivo instanceof Ligavel){
+				((Ligavel) dispositivo).ligar();
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean desligar(String nome) {
+		for (Dispositivo dispositivo : dispositivos) {
+			if (((Amplexa) dispositivos).getNome().equals(nome) && dispositivo instanceof Ligavel){
+				((Ligavel) dispositivo).desligar();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int aumentarPotencia(String nome) {
+		for (Dispositivo dispositivo : dispositivos) {
+			if (dispositivo.nome.equals(nome) && dispositivo instanceof PotenciaAjustavel) {
+				return ((PotenciaAjustavel) dispositivo).aumentarPotencia(0);
+			}
+		}
+		return -1;
+	}
 }
